e2ec54e1e294a3a07dbcc8dae48497763a5ace54
sanofficial825-dev <sanofficial825@gmail.com>
2025-12-06 15:39:37 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// export const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Needed for Neon SSL
+  },
 });
+
