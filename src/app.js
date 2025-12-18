5fbe6307504bed5cfec469c25920e37cedc2ab86
sanofficial825-dev <sanofficial825@gmail.com>
2025-12-18 18:44:07 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import productRoutes from "./routes/product.routes.js";
 import brandRoutes from "./routes/brand.routes.js";
+import ingredientRoutes from "./routes/brand.routes.js";
 import commonRoutes from "./routes/common.routes.js";
 
 const app = express();
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use("/products", productRoutes);
 
 app.use("/brands", brandRoutes);
+
+app.use("/ingredients", ingredientRoutes);
 
 app.use("/", commonRoutes);
 
@@ -25,9 +28,11 @@ export default app;
 // getProduct by pagination: http://localhost:8000/products?page=1&limit=20
 // Multiple filters can be applied together: http://localhost:8000/products?brand=someBrand1,someBrand2&category=someCategory1,someCategory2&search=someText&page=2&limit=10&id=100
 
+// get products by ingredient name and dosage: http://localhost:8000/products/byIngredient?name=Acai&dosage=HIGH
 
 // =========== get Brands ===========//
 // example: http://localhost:8000/brands/
+// example with search param: http://localhost:8000/brands?search=gn
 
 
 // =========== get Product Names by Brand Name ===========//
@@ -36,3 +41,8 @@ export default app;
 
 // =========== get Category Names ===========//
 // example: http://localhost:8000/getCategoryNames
+
+
+// =========== get Ingredients ===========//
+// example: http://localhost:8000/ingredients/
+// example with search param: http://localhost:8000/ingredients?search=rose
